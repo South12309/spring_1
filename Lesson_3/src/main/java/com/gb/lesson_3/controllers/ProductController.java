e5f716b52cd96bab67389b2ee8000754842edca5
South <south123@mail.ru>
2022-11-09 13:36:18 +0300

    Lesson 3.back is done. Need to do front

--- Lesson_3/src/main/java/com/gb/lesson_3/controllers/ProductController.java
+++ Lesson_3/src/main/java/com/gb/lesson_3/controllers/ProductController.java
@@ -1,13 +1,18 @@
 package com.gb.lesson_3.controllers;
 
+import com.gb.lesson_3.models.Product;
 import com.gb.lesson_3.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
 
@@ -17,7 +22,22 @@ public class ProductController {
     }
 
     @GetMapping
-    public String getAllProducts() {
-        
+    public String getAllProducts(Model model) {
+        List<Product> allProducts = productService.getAllProducts();
+        model.addAttribute("products", allProducts);
+        return "products";
+    }
+
+    @GetMapping("/new")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "new_product";
+    }
+
+    @PostMapping("/new")
+    public String addProduct(Model model) {
+        Product product = (Product)model.getAttribute("product");
+        productService.addProduct(product);
+        return "redirect:products";
     }
 }
--- Lesson_3/src/main/java/com/gb/lesson_3/models/Product.java
+++ Lesson_3/src/main/java/com/gb/lesson_3/models/Product.java
@@ -2,10 +2,12 @@ package com.gb.lesson_3.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class Product {
     private Long id;
